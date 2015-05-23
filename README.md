719634b60e8a5ecb7ef04aabf37a4c6dfd011302
swhiteley91 <simonwhiteley@hotmail.com>
2015-05-24 01:22:15 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# dotNetEindopdracht
+
+## MySQL Visual Studio Plugin 1.1.1
+https://cdn.mysql.com/Downloads/MySQLInstaller/mysql-visualstudio-plugin-1.1.1.msi
+
+## MySQL Connect .NET 6.8.3
+http://dev.mysql.com/downloads/file.php?id=450594
---
+++ WcfServiceLibrary1/PracticumService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class PracticumService : IPracticumService
+    {
+netpracticumEntities dbContext = new netpracticumEntities();
+
+        public string Register(string givenUsername)
+        {
+            char[] passwordCharArray = givenUsername.ToCharArray();
+            Array.Reverse(passwordCharArray);
+            string strPassword = new string(passwordCharArray);
+
+            user newUser = new user();
+            newUser.username = givenUsername;
+            newUser.password = strPassword;
+            newUser.saldo = 100;
+
+            dbContext.users.Add(newUser);
+            dbContext.SaveChanges();
+            
+            user u = dbContext.users.Find(4);
+            Debug.WriteLine(u.username);
+
+            return strPassword;
+        }
+
+        public bool LogIn(string username, string password)
+        {
+            try
+            {
+                user user = (from u in dbContext.users
+                             where u.username == username && u.password == password
+                             select u).FirstOrDefault();
+                Debug.WriteLine("username: " + user.username + ", password: " + user.password);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return false;
+        }
+    }
+}
